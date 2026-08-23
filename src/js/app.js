b63f50c2222406733419b12e44737c7f2b4eb516
Luiz Gustavo Sampaio dos Santos <luiz.sampaio@aluno.unc.br>
2026-08-22 23:20:52 -0300

    feat: criando function deletarTerafa()

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,15 @@ function adicionarTarefa() {
     let tarefaInput = document.querySelector("input").value
     let li = createElement("li")
 
-    li.innerHTML = tarefaInput + "<span>❌</span>"
+    li.innerHTML = tarefaInput + '<span onclick="deletarTarefa()">❌</span>'
 
     console.log(li)
     
     document.querySelector("ul").appendChild(li)
 
-    tarefaInput = ""
+     document.querySelector("input").value = ""
 }
 
+function deletarTarefa() {
+    li.ParennrElement.remove()
+}
